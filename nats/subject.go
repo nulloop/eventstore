20aaacc548a3284eb448a7528e4fb4ad2a766022
Ali Najafizadeh <a.najafizadeh@gmail.com>
2018-06-29 00:00:33 -0400

    added subject

--- nats/subject.go
+++ nats/subject.go
@@ -22,6 +22,24 @@ func (n *Subject) Topic() string {
 	return n.topic
 }
 
+func (n *Subject) Clone(options ...Option) (*Subject, error) {
+	subject, err := NewSubject(n.topic)
+	if err != nil {
+		return nil, err
+	}
+
+	subject.durable = n.durable
+	subject.queue = n.queue
+	subject.sequence = n.sequence
+	subject.msgInstance = n.msgInstance
+
+	for _, option := range options {
+		option(subject)
+	}
+
+	return subject, nil
+}
+
 type Option func(*Subject)
 
 func OptQueueName(name string) Option {
